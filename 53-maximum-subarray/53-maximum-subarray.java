bd05d280ba80e993cfed7b0cf4162c0698a514d4
Khushi Saxena <khushisaxena123123@gmail.com>
2022-01-31 19:33:49 +0530

    Time: 3 ms (25.66%), Space: 81.8 MB (51.24%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int maxValue = nums[0],sum=0;
+        for(int n:nums){
+            sum += n;
+            if(sum >= maxValue){
+                maxValue = sum;
+            }
+            if(sum < 0){
+                sum=0;    
+            }
+        }
+        return maxValue;    
+    }
+}
